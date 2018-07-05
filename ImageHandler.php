a0c93f0dd3a90fa20b2ca1dc7df8c741a4fe5885
raiika <33516651+raiika@users.noreply.github.com>
2018-07-05 13:34:56 +0800

    Update ImageHandler.php

--- ImageHandler.php
+++ ImageHandler.php
@@ -5,47 +5,8 @@ namespace App\Traits;
 use Intervention\Image\ImageManager;
 use Illuminate\Support\Facades\File;
 
-
-/*
-
-in Controller
-
-$model = new Model();
-$model->image = $request;
-
-*/
-
 trait ImageHandler
 {
-	/* ALL AVAILABLE CONFIG
-	protected $imageHandler = [
-		'dir' => 'images/sliders', 
-		'thumbFolder' => 'thumbs', 
-		'useThumb' => false, 
-		'dimension' => [
-			'w' => 500, 
-			'h' => 200
-		],
-		'thumbDimension' => [
-			'w' => 400, 
-			'h' => 200
-		],
-		'ratioVar' => [
-		 	'x' => 'x',
-		 	'y' => 'y',
-		 	'w' => 'w',
-		 	'h' => 'h',
-		],
-		'defaultRatio' => [
-		 	'x' => 0,
-		 	'y' => 0,
-		 	'w' => 500,
-		 	'h' => 500,
-		],
-		'name' => 'image'
-	]
-	*/
-
 	public function setImageAttribute($request)
 	{
         $filename = $this->getFilename($request);
